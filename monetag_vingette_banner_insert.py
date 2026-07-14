79d13ff9a41b45a40118f346cb9ce616082f3d35
dev-mthrpankaj <79391058+dev-mthrpankaj@users.noreply.github.com>
2026-07-14 22:16:50 +0530

    change

--- monetag_vingette_banner_insert.py
+++ monetag_vingette_banner_insert.py
@@ -1,7 +1,7 @@
 import os
 
 # ===== CONFIG =====
-TARGET_FOLDER = r"D:\GovJovInd\AnswerKey_Details\HTML"
+TARGET_FOLDER = r"D:\GovJovInd\Job_Details\HTML"
 SCRIPT_TAG = '<script>(function(s){s.dataset.zone=\'11274653\',s.src=\'https://n6wxm.com/vignette.min.js\'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement(\'script\')))</script>'
 # ===================
 
