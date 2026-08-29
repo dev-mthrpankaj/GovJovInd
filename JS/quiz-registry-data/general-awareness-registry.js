a7fe4ce9a4ecc8b06cda837aceaf5a4efece88e9
dev-mthrpankaj <79391058+dev-mthrpankaj@users.noreply.github.com>
2026-08-29 14:46:03 +0530

    change

--- JS/quiz-registry-data/general-awareness-registry.js
+++ JS/quiz-registry-data/general-awareness-registry.js
@@ -6,15 +6,5 @@
 
     const { quizMeta, registerSubjectQuizzes } = helpers;
 
-    registerSubjectQuizzes([
-        quizMeta(
-            "general-awareness-bilingual-latex-set-1",
-            "General Awareness",
-            "General Awareness Bilingual LaTeX Set 1",
-            "Hindi-English bilingual General Awareness quiz with rich-text and MathJax-safe formatting.",
-            "Moderate",
-            "quiz-data/general-awareness/general-awareness-bilingual-latex-set-1.js",
-            10
-        )
-    ]);
+    registerSubjectQuizzes([]);
 }());
